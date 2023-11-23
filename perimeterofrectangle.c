38ac47b6558a8b5b29be65b4cb0d5d562efe3d28
Zeno_Harsh <118764173+Only13harsh@users.noreply.github.com>
2023-11-23 20:47:26 +0530

    Create perimeterofrectangle.c

---
+++ perimeterofrectangle.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+  int a ;
+int b;
+printf("enter the length of the rectngle :");
+scanf("%d ",&a);
+
+printf("enter the bredth of the rectngle :");
+scanf("%d ",&b);
+
+int c = 2(a+b);
+
+printf("the perimeter of the rectangle :" ,&c);
+
+return 0;
+}
