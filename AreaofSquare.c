02785233e7bed66a3f8089612a16f2b48b7e4981
Zeno_Harsh <118764173+Only13harsh@users.noreply.github.com>
2023-12-07 19:20:21 +0530

    AreaofSquare.c
    this program calculates the area of a square by user inputing one of its side

---
+++ AreaofSquare.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+int side ;
+printf("enter the side of the given square to find out the area \n");
+scanf("%d",&side);
+side = side * side;
+printf(" the area of the given square is ",side);
+return 0;
+}
