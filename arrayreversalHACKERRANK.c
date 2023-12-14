56841418eff033dc196f774cab6b93177547fa4b
Zeno_Harsh <118764173+Only13harsh@users.noreply.github.com>
2023-12-14 20:31:57 +0530

    Solution of Hackerrank array reversal
    Given an array, of size , reverse it.
    
    Example: If array, , after reversing it, the array should be, .
    
    Input Format
    
    The first line contains an integer, , denoting the size of the array. The next line contains  space-separated integers denoting the elements of the array.
    
    Constraints
    
    
    , where  is the  element of the array.
    
    Output Format
    
    The output is handled by the code given in the editor, which would print the array.
    
    Sample Input 0
    
    6
    16 13 7 2 1 12 
    Sample Output 0
    
    12 1 2 7 13 16 
    Explanation 0
    
    Given array,  = . After reversing the array,  = 
    
    Sample Input 1
    
    7
    1 13 15 20 12 13 2 
    Sample Output 1
    
    2 13 12 20 15 13 1 
    Sample Input 2
    
    8
    15 5 16 15 17 11 5 11 
    Sample Output 2
    
    11 5 11 17 15 16 5 15

---
+++ arrayreversalHACKERRANK.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+
+    // Input array elements
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Reverse the array
+    for (int i = 0; i < n / 2; i++) {
+        int temp = arr[i];
+        arr[i] = arr[n - 1 - i];
+        arr[n - 1 - i] = temp;
+    }
+
+    // Print the reversed array
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
