076dc68502e455b55e7c33228db61a08401c9756
Zeno_Harsh <118764173+Only13harsh@users.noreply.github.com>
2023-12-22 20:36:38 +0530

    HRifelseusingjava.java
    this program is a simple demonstrataion of using if else in java

---
+++ HRifelseusingjava.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        if (n % 2 != 0) {
+            System.out.println("Weird");
+        } else {
+            if (n >= 2 && n <= 5) {
+                System.out.println("Not Weird");
+            } else if (n >= 6 && n <= 20) {
+                System.out.println("Weird");
+            } else {
+                System.out.println("Not Weird");
+            }
+        }
+        scanner.close();
+    }
+}
