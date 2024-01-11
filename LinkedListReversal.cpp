3ea7ee37926343692b021976ea3894cbe0149473
Zeno_Harsh <118764173+Only13harsh@users.noreply.github.com>
2024-01-11 22:27:44 +0530

    LinkedListReversal.cpp
    This program reverses the linked list

---
+++ LinkedListReversal.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+
+using namespace std;
+
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+// Function to reverse a linked list
+ListNode* reverseList(ListNode* head) {
+    ListNode* prev = nullptr;
+    ListNode* current = head;
+    ListNode* nextNode = nullptr;
+
+    while (current != nullptr) {
+        nextNode = current->next;
+        current->next = prev;
+        prev = current;
+        current = nextNode;
+    }
+
+    return prev;
+}
+
+// Function to print the linked list
+void printList(ListNode* head) {
+    while (head != nullptr) {
+        cout << head->val << " ";
+        head = head->next;
+    }
+    cout << endl;
+}
+
+int main() {
+    // Example usage
+    ListNode* head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    head->next->next->next = new ListNode(4);
+    head->next->next->next->next = new ListNode(5);
+
+    cout << "Original Linked List: ";
+    printList(head);
+
+    head = reverseList(head);
+
+    cout << "Reversed Linked List: ";
+    printList(head);
+
+    // Cleanup: Free memory allocated for the linked list
+    while (head != nullptr) {
+        ListNode* temp = head;
+        head = head->next;
+        delete temp;
+    }
+
+    return 0;
+}
