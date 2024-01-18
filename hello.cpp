3fe913ca2ffcf9174326fde9252f6097b86fd7b0
Zeno_Harsh <118764173+Only13harsh@users.noreply.github.com>
2024-01-18 22:26:39 +0530

    hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "H";
+    cout << "e";
+    cout << "l";
+    cout << "l";
+    cout << "o";
+    cout << endl;
+
+    return 0;
+}
