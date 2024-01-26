75260a6ea39613da558ca002a704c44e9905869d
Zeno_Harsh <118764173+Only13harsh@users.noreply.github.com>
2024-01-26 23:13:56 +0530

    FirstPython.py
    My first program in Python and it includes all the basics that i was taught in the class today.

---
+++ FirstPython.py
@@ -0,0 +1,59 @@
+print("hello world")
+age=22
+Name="harsh"
+print(age)
+print(Name)
+x=2
+y=3
+z=4
+print(x,y,z)
+print(type (x))
+print(type (y))
+print(type (z))
+print(type (Name))
+myVariable=20
+print(myVariable)
+MyVariable30=60
+print(MyVariable30)
+my_variable_name="harsh"
+print(my_variable_name)
+a=b=c="Apple"
+print(a)
+print(b)
+print(c)
+a=b=c="Apple","banana","Orange"
+print(a)
+print(b)
+print(c)
+fruits=["Apple","orange","banana"]
+d,e,f=fruits
+print(d)
+print(e)
+print(f)
+print("Welcome to LPU")
+j="Welcome to LPU"
+print(j)
+g="python"
+h="is"
+i="fun"
+print(g,h,i)
+"""hey this is fang fun fis from fiana
+i just want to tell you that you are awefome
+"""
+num1=10
+num2=20
+result=num1+num2
+print(result)
+
+num3=10
+num4="alok"
+print(num3,num4)
+
+#single line comment
+if 5>2:
+     print("Condition is true")
+
+if (5>4):
+    print("5 is greater then 2")
+else:
+    print("2 is greater then 5")
