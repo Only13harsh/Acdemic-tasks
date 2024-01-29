36865e8c25731ae0a73290e5fc84d3f2377ed5b0
Zeno_Harsh <118764173+Only13harsh@users.noreply.github.com>
2024-01-29 23:28:08 +0530

    EvenOdd.r
     Simple R code to check if a number is even or odd using Function to check if a number is even or odd

---
+++ EvenOdd.r
@@ -0,0 +1,9 @@
+check_even_odd <- function(number) {
+  if (number %% 2 == 0) {
+    cat("The entered number is even.\n")
+  } else {
+    cat("The entered number is odd.\n")
+  }
+}
+user_input <- as.numeric(readline("Enter a number: "))
+check_even_odd(user_input)
