59f4db0ec64c5a69d20c915b837f15221263383e
Zeno_Harsh <118764173+Only13harsh@users.noreply.github.com>
2024-02-04 14:51:19 +0530

    EvenOddInR.r
    A program in R langauge to print odd even no upto 100

---
+++ EvenOddInR.r
@@ -0,0 +1,10 @@
+printEvenOddNumbers <- function(limit) {
+  for (i in 1:limit) {
+    if (i %% 2 == 0) {
+      cat(i, "is an even number\n")
+    } else {
+      cat(i, "is an odd number\n")
+    }
+  }
+}
+printEvenOddNumbers(100)
