649e78f97904076a1688180bb2a37f3c0e378a3c
Zeno_Harsh <118764173+Only13harsh@users.noreply.github.com>
2024-02-09 14:51:30 +0530

    LedPullUP.py
    Pull up circuit for esp32

---
+++ LedPullUP.py
@@ -0,0 +1,13 @@
+factorial <- function(n) {
+  if (n <= 0) {
+    return(1)
+  }
+  return(n * factorial(n - 1))
+}
+n <- as.integer(readline(prompt = "Enter a number to find the factorial: "))
+result_fact <- factorial(n)
+cat("The factorial of", n, "is", result_fact)
+
+n <- as.integer(readline(prompt = "Enter a number to find the factorial: "))
+result_fact <- factorial(n)
+cat("The factorial of", n, "is", result_fact)
