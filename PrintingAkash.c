6a1bf5a25912810f9dd71bf65b98fc0c35f09e98
Zeno_Harsh <118764173+Only13harsh@users.noreply.github.com>
2024-02-15 21:50:52 +0530

    PrintingAkash.cpp
    printing the name akash digonally

---
+++ PrintingAkash.c
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Diagonal Printing of the Name 'Akash':" << endl;
+
+    cout << "A" << endl;
+    cout << "  k" << endl;
+    cout << "    a" << endl;
+    cout << "      s" << endl;
+    cout << "        h" << endl;
+
+    return 0;
+}
