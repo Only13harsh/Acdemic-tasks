2fcdbbc2583013142e2b23191e0b507df4051d79
Zeno_Harsh <118764173+Only13harsh@users.noreply.github.com>
2024-04-03 00:39:38 +0530

    custon lcd.ino

---
+++ custon lcd.ino
@@ -0,0 +1,23 @@
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2); // RS, E, D4, D5, D6, D7
+
+byte customChar[] = {
+  B00000,
+  B00000,
+  B00000,
+  B00000,
+  B00000,
+  B00000,
+  B00000,
+  B00000
+};
+
+void setup() {
+  lcd.begin(16, 2);
+  lcd.createChar(0, customChar);
+  lcd.home();
+  lcd.write(0);
+}
+
+void loop() { }
