63982f37a1c1bd9da223f4657ad279ac553ab904
Zeno_Harsh <118764173+Only13harsh@users.noreply.github.com>
2024-04-13 23:16:35 +0530

    oled.py

---
+++ oled.py
@@ -0,0 +1,17 @@
+from machine import Pin, SoftI2C
+from ssd1306 import SSD1306_I2C
+from time import sleep
+
+# ESP32 Pin assignment
+i2c = SoftI2C(scl=Pin(22), sda=Pin(21), freq=100000)
+
+I2C_ADDR = 0x3c
+oled_width = 128
+oled_height = 64
+oled = SSD1306_I2C(oled_width, oled_height, i2c, I2C_ADDR)
+oled.invert(True)
+oled.pixel(21,24,1)
+oled.text('Hello, World 1!', 0, 0, 1)
+oled.text('Embedded Systems', 7, 8)
+oled.text('Micro Python', 14, 16)
+oled.show()
