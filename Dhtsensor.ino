479545a14bacd89346d3b81bfce38d0333b218dc
Zeno_Harsh <118764173+Only13harsh@users.noreply.github.com>
2024-04-16 22:55:15 +0530

    Dhtsensor.ino

---
+++ Dhtsensor.ino
@@ -0,0 +1,32 @@
+#include "DHT.h"
+
+#define DHTPIN 2     // what digital pin the DHT11 is connected to
+#define DHTTYPE DHT11   // DHT 11
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("DHT11 test!");
+
+  dht.begin();
+}
+
+void loop() {
+  delay(2000);
+
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  Serial.print("Humidity: "); 
+  Serial.print(h);
+  Serial.print(" %\t");
+  Serial.print("Temperature: "); 
+  Serial.print(t);
+  Serial.println(" *C");
+}
