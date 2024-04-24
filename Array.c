32a3e7001c2a36927f2e4fdf17857342df642b58
Zeno_Harsh <118764173+Only13harsh@users.noreply.github.com>
2024-04-25 00:03:56 +0530

    Array.c

---
+++ Array.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int arr[5] = {1, 2, 3, 4, 5};
+    int sum = 0;
+
+    for (int i = 0; i < 5; i++) {
+        sum += arr[i];
+    }
+
+    printf("Sum of array elements: %d\n", sum);
+
+    return 0;
+}
