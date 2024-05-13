ea9ab8bb01f7e845c113255f743558d4b8496eef
Zeno_Harsh <118764173+Only13harsh@users.noreply.github.com>
2024-05-13 17:58:08 +0530

    Create Reaarnage.r

---
+++ Reaarnage.r
@@ -0,0 +1,9 @@
+arrange_ascending <- function(numbers) {
+  sorted_numbers <- sort(numbers)
+  return(sorted_numbers)
+}
+
+# Example usage:
+numbers <- c(5, 2, 8, 1, 9)
+arranged_numbers <- arrange_ascending(numbers)
+print(arranged_numbers)
