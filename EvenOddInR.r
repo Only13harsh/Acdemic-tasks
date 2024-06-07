264d1aefab9389ec6a1512f7d4b69bb7d7c7e993
Zeno_Harsh <118764173+Only13harsh@users.noreply.github.com>
2024-06-07 13:48:05 +0530

    Update EvenOddInR.r

--- EvenOddInR.r
+++ EvenOddInR.r
@@ -3,7 +3,7 @@ printEvenOddNumbers <- function(limit) {
     if (i %% 2 == 0) {
       cat(i, "is an even number\n")
     } else {
-      cat(i, "is an odd number\n")
+      cat(i, "is an odd  number\n")
     }
   }
 }
